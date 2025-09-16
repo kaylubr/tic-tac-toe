4453a9b0c5494343eaa734173b7cf3b89940bc8f
kaylubr <kylebreyes092005@gmail.com>
2025-09-16 20:58:09 +0800

    Add functionality where a user can restart the game when there's a winner or a tie

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ const Gameboard = (function() {
   const COLUMNS = 3;
   const board = [];
 
-  for (let row = 0; row < ROWS; row++) {
-    board[row] = []
-    for (let column = 0; column < COLUMNS; column++) {
-      board[row][column] = Cell();
+  const generateNewBoard = () => {
+    console.log('runs again');
+     
+    for (let row = 0; row < ROWS; row++) {
+      board[row] = []
+      for (let column = 0; column < COLUMNS; column++) {
+        board[row][column] = Cell();
+      }
     }
   }
+
+  generateNewBoard();
 
   const addMark = (row, column, mark) => {
     const isCellAvailable = board[row][column].getMark() === '';
@@ -30,7 +36,7 @@ const Gameboard = (function() {
     console.log(boardToPrint);
   }
 
-  return { addMark, getBoard, printBoard }
+  return { addMark, getBoard, generateNewBoard ,printBoard }
 })();
 
 const Player = function(name, mark) {
@@ -49,16 +55,17 @@ const GameController = (function() {
   const board = Gameboard;
 
   let activePlayer = P1;
-
   const getActivePlayer = () => activePlayer;
-
   const switchActivePlayer = () => {
     activePlayer = activePlayer === P1 ? P2 : P1;
   }
 
   const getWinner = () => winner;
-  const resetWinner = () => winner = null;
-
+  const resetWinner = () => {
+    winner = null;
+    activePlayer = P1;
+  }
+  
   const checkWin = (activePlayer) => {
     const WIN_PATTERNS = [
       [0, 1, 2],
@@ -103,6 +110,7 @@ const GameController = (function() {
     playRound,
     nameSetter: { p1: P1.setName, p2: P2.setName },
     board: Gameboard.getBoard(),
+    generateNewBoard: Gameboard.generateNewBoard,
     getActivePlayer,
     getWinner,
     resetWinner
@@ -113,21 +121,14 @@ const GameController = (function() {
 const ScreenController = (function() {
   const game = GameController;
   const boardDiv = document.querySelector('.board');
-  const dialog = document.querySelector('#startDialog');
+  const startDialog = document.querySelector('#startDialog');
+  const endDialog = document.querySelector('#endDialog');
   const startBtn = document.querySelector('#startDialog button');
+  const playAgainBtn = document.querySelector('#playAgainBtn');
+  const refreshBtn = document.querySelector('#refreshBtn');
   const playerTurn = document.querySelector('#playerTurn');
   const P1 = document.querySelector('#player1');
   const P2 = document.querySelector('#player2');
-
-  dialog.showModal();
-
-  startBtn.addEventListener('click', () => {
-    if (P1.value === '' || P2.value === '') return;
-    game.nameSetter.p1(P1.value);
-    game.nameSetter.p2(P2.value);
-    playerTurn.textContent = `Player ${game.getActivePlayer().getName()}'s turn`;
-    dialog.close();
-  })
 
   const updateScreen = () => {
     boardDiv.textContent = '';
@@ -146,52 +147,79 @@ const ScreenController = (function() {
     });
   }
 
-  boardDiv.addEventListener('click', e => {
-    // Change content of the cell that was clicked
-    const cell = e.target;
-    const row = cell.dataset.row;
-    const col = cell.dataset.column;
-
-    if (cell.textContent !== '') return;
-
-    cell.textContent = game.getActivePlayer().getMark();
-
-    // Play the round 
-    game.playRound(row, col);
-    playerTurn.textContent = `Player ${game.getActivePlayer().getName()}'s turn`;
-
-    // Renders winner or tie screen if there is any
-    if (game.getWinner() && game.getWinner() !== 'TIE') {
-      renderWinner('win');
-      return;
-    } else if (game.getWinner() === 'TIE') {
-      renderWinner('tie');
-      return;
-    }
-    
-    // Reset screen
-    updateScreen();
-  });
-
-  const renderWinner = (mode) => {
-
-    // Reset board
-    boardDiv.textContent = '';
-    const endDialog = document.querySelector('#endDialog');
-    const endHeader = document.querySelector('#winnerPlayer');
-    const gif = document.querySelector('#gif');
-
-    if (mode === 'win') {
-      endHeader.textContent = `${game.getActivePlayer().getName()} won the game!`;
-      gif.src = './gif/cat-celebration.gif';
-      endDialog.showModal();
-    } else if (mode === 'tie') {
-      endHeader.textContent = "It's a tie!";
-      gif.src = './gif/james-doakes.gif';
-      endDialog.showModal();
-    }
-
+  const reset = () => {
+    game.generateNewBoard();
+    game.resetWinner();
+    P1.value = '';
+    P2.value = '';
   }
 
-  updateScreen();
+  const play = () => {
+    startDialog.showModal();
+  
+    startBtn.addEventListener('click', () => {
+      if (P1.value === '' || P2.value === '') return;
+      game.nameSetter.p1(P1.value);
+      game.nameSetter.p2(P2.value);
+      playerTurn.textContent = `Player ${game.getActivePlayer().getName()}'s turn`;
+      updateScreen();
+      startDialog.close();
+    });
+
+    playAgainBtn.addEventListener('click', () => {
+      endDialog.close();
+      reset();
+      startDialog.showModal();
+    });
+  
+    boardDiv.addEventListener('click', e => {
+      // Change content of the cell that was clicked
+      const cell = e.target;
+      const row = cell.dataset.row;
+      const col = cell.dataset.column;
+  
+      if (cell.textContent !== '') return;
+  
+      cell.textContent = game.getActivePlayer().getMark();
+  
+      // Play the round 
+      game.playRound(row, col);
+      playerTurn.textContent = `Player ${game.getActivePlayer().getName()}'s turn`;
+  
+      // Renders winner or tie screen if there is any
+      if (game.getWinner() && game.getWinner() !== 'TIE') {
+        renderWinner('win');
+        return;
+      } else if (game.getWinner() === 'TIE') {
+        renderWinner('tie');
+        return;
+      }
+      
+      // Reset screen
+      updateScreen();
+    });
+  
+    const renderWinner = (mode) => {
+  
+      // Reset board
+      boardDiv.textContent = '';
+      const endHeader = document.querySelector('#winnerPlayer');
+      const gif = document.querySelector('#gif');
+  
+      if (mode === 'win') {
+        endHeader.textContent = `${game.getActivePlayer().getName()} won the game!`;
+        gif.src = './gif/cat-celebration.gif';
+        endDialog.showModal();
+      } else if (mode === 'tie') {
+        endHeader.textContent = "It's a tie!";
+        gif.src = './gif/james-doakes.gif';
+        endDialog.showModal();
+      }
+  
+    }
+  
+    updateScreen();
+  }
+
+  play();
 })();
