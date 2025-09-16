1353eb080b77d292542bbc090a1c201428a7a7c0
kaylubr <kylebreyes092005@gmail.com>
2025-09-16 20:18:12 +0800

    Add functionality where a dialog is opened when there's a winner

--- script.js
+++ script.js
@@ -44,14 +44,20 @@ const Player = function(name, mark) {
 const GameController = (function() {
   const P1 = Player('John', 'X');
   const P2 = Player('Doe', 'O');
+  let winner = null;
 
   const board = Gameboard;
 
   let activePlayer = P1;
 
+  const getActivePlayer = () => activePlayer;
+
   const switchActivePlayer = () => {
     activePlayer = activePlayer === P1 ? P2 : P1;
   }
+
+  const getWinner = () => winner;
+  const resetWinner = () => winner = null;
 
   const checkWin = (activePlayer) => {
     const WIN_PATTERNS = [
@@ -73,28 +79,33 @@ const GameController = (function() {
       const pattern3 = pattern[2];
       
       if (mappedBoard[pattern1] !== '' && mappedBoard[pattern1] === mappedBoard[pattern2] && mappedBoard[pattern1] === mappedBoard[pattern3]) {
-        console.log(`${activePlayer.getName()} WON!!`);
+        winner = getActivePlayer().getName();
         return;
       } 
     }
 
     if (!mappedBoard.includes('')) {
-      console.log('TIE');
+      winner = 'TIE';
       return;
     }
   }
 
   const playRound = (row, column) => {
-    board.addMark(row, column, activePlayer.getMark());
-    checkWin(activePlayer);
-    switchActivePlayer();
+    board.addMark(row, column, getActivePlayer().getMark());
+    checkWin();
+    if (!winner) {
+      switchActivePlayer();
+    }
     board.printBoard();
   }
 
   return { 
     playRound,
     nameSetter: { p1: P1.setName, p2: P2.setName },
-    board: Gameboard.getBoard()
+    board: Gameboard.getBoard(),
+    getActivePlayer,
+    getWinner,
+    resetWinner
   }
 })();
 
@@ -104,6 +115,7 @@ const ScreenController = (function() {
   const boardDiv = document.querySelector('.board');
   const dialog = document.querySelector('#startDialog');
   const startBtn = document.querySelector('#startDialog button');
+  const playerTurn = document.querySelector('#playerTurn');
   const P1 = document.querySelector('#player1');
   const P2 = document.querySelector('#player2');
 
@@ -113,22 +125,58 @@ const ScreenController = (function() {
     if (P1.value === '' || P2.value === '') return;
     game.nameSetter.p1(P1.value);
     game.nameSetter.p2(P2.value);
+    playerTurn.textContent = `Player ${game.getActivePlayer().getName()}'s turn`;
     dialog.close();
   })
 
   const updateScreen = () => {
+    boardDiv.textContent = '';
+
     const board = game.board;
     
     board.forEach((row, rowIndex) => {
       row.forEach((cell, colIndex) => {
         const button = document.createElement('button');
         button.classList.add('cells');
-        button.dataset.column = rowIndex;
-        button.dataset.row = colIndex;
+        button.dataset.row = rowIndex;
+        button.dataset.column = colIndex;
         button.textContent = cell.getMark();
         boardDiv.append(button);
       })
-    })
+    });
+  }
+
+  boardDiv.addEventListener('click', e => {
+    // Change content of the cell that was clicked
+    const cell = e.target;
+    const row = cell.dataset.row;
+    const col = cell.dataset.column;
+    cell.textContent = game.getActivePlayer().getMark();
+
+    // Play the round 
+    game.playRound(row, col);
+    playerTurn.textContent = `Player ${game.getActivePlayer().getName()}'s turn`;
+
+    // Reset screen
+    if (game.getWinner() && game.getWinner() !== 'TIE') {
+      renderWinner('win');
+    }
+
+    updateScreen();
+  });
+
+  const renderWinner = (mode) => {
+
+    // Reset board
+    boardDiv.textContent = '';
+
+    if (mode === 'win') {
+      const endDialog = document.querySelector('#endDialog');
+      const endHeader = document.querySelector('#winnerPlayer');
+      endHeader.textContent = `${game.getActivePlayer().getName()} won the game!`;
+      endDialog.showModal();
+    }
+
   }
 
   updateScreen();
